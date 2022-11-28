ebf7892d42889df25312638d290fe5f261835965
Igor Melnichenko <imelnicenko498@gmail.com>
2022-11-28 20:15:21 +0300

    Lang Syntax ready

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        char[] arrayOfNumbers = Integer.toString(number).toCharArray();
+        int result = 0;
+        for (char x: arrayOfNumbers){
+            result += (x - '0');
+        }
+        System.out.println(result);
     }
 
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,10 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int firstChar = number/100;
+        int secondChar = (number/10)%10;
+        int tripleChar = (number%100)%10;
+        System.out.println(tripleChar+""+secondChar+""+firstChar);
     }
 
 }
