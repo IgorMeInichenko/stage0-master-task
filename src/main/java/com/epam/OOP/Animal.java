00909be5c327f81f5ed4b08450b6a836cb60cca3
Igor Melnichenko <imelnicenko498@gmail.com>
2022-11-28 19:44:02 +0300

    Ready "Animals"

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,30 @@
 package com.epam.OOP;
 
 public class Animal {
-    String tegghotr;
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription(){
+        String result = "This animal is mostly "+this.color+". It has ";
+        if (this.numberOfPaws>1){
+            result += this.numberOfPaws;
+            result +=" paws and";
+        }
+        else {
+            result += "1 paw and";
+        }
+        if (this.hasFur){
+            result += " a fur.";
+        }
+        else {
+            result += " no fur.";
+        }
+        return result;
+    }
 }
