215619dca03e808afb0ac4979799e8ce318bc000
Igor Melnichenko <imelnicenko498@gmail.com>
2022-11-28 20:40:29 +0300

    Task 3 Conditions ready

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,17 @@
 package com.epam.conditions;
+import java.time.YearMonth;
 
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+        if (year>0 && (0<month) && (month<13)){
+            YearMonth yearMonthObject = YearMonth.of(year, month);
+            int daysInMonth = yearMonthObject.lengthOfMonth();
+            System.out.println(daysInMonth);
+        }
+        else {
+            System.out.println("invalid date");
+        }
     }
 
 }
--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,14 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (checkSides(firstSide, secondSide, thirdSide)){
+            System.out.println("this is a valid triangle");
+        }
+        else {
+            System.out.println("it's not a triangle");
+        }
     }
-
+    boolean checkSides(double x1, double x2, double x3){
+        return (x1+x2 > x3)&&(x3+x2 > x1)&&(x1+x3 > x2);
+    }
 }
